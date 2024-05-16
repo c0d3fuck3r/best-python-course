d14775a7021091b0438c83e0a9cbf842620a2ae1
m3xan1k <m3xan1k@duck.com>
2024-05-16 14:32:33 +0300

    backuper project

---
+++ 05_backuper.py
@@ -0,0 +1,29 @@
+# Explain os module
+
+import os
+import shutil
+
+
+if __name__ == "__main__":
+
+    src_dir = os.getcwd()
+    dst_dir = os.path.join(src_dir, "backup")
+
+    # for file in os.listdir(src_dir):
+    #     print(file)
+
+    if not os.path.exists(dst_dir):
+        print(f"Backup dir {dst_dir} does not exist.")
+        print("Creating...")
+        os.mkdir(dst_dir)
+
+    total_bytes = 0
+
+    for file in os.listdir(src_dir):
+        if not os.path.isdir(file):
+            total_bytes += os.path.getsize(file)
+            shutil.copy(file, dst_dir)
+
+    print("Backup completed.")
+    total_mb = "%.2f" % (total_bytes / 1024 / 1024)
+    print(f"{total_mb} MB copied.")
